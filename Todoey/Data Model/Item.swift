282c014b2be76c5cdc98cf2a89178ff204bd767b
Chris Heder <chris@chrisheder.com>
2019-07-09 10:12:57 -0400

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Chris Heder on 7/8/19.
+//  Copyright © 2019 Chris Heder. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
